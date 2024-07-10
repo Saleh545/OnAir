fc1feab7aff58dad02d02925884b4b28f213da80
Saleh545 <syusifzade545@gmail.com>
2024-07-10 18:30:35 +0400

    series page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,32 +29,34 @@ const Home = () => {
       <Slider {...settings}>
         <div className="relative"  >
           <img src={homebck} alt="" className="z-10" />
-          <div className="absolute inset-0 flex justify-center items-center flex-col w-[35%] font-light ">
-            <h2 className="z-40 text-white text-center text-5xl">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
-          <p className="text-white px-[34px] cursor-pointer text-2xl font-medium py-[10px] rounded-full bg-[#E13C52] mt-[70px] hover:bg-[#f46174] ">Start FREE trial</p>
+          <div className="lg:absolute md:absolute sm:static inset-0 flex  justify-center items-center flex-col lg:w-[35%] sm:w-[100%] md:w-[55%] px-9 sm:mt-5   mt-5 w-full font-light">
+          <h2 className="z-40 text-white text-center lg:text-5xl text-[30px] md:text-[35px] sm:text-[35px]">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
+          <p className="text-white px-[34px] cursor-pointer lg:text-2xl  md:text-[20px] sm:text-[20px] font-medium py-[10px] rounded-full bg-[#E13C52] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] hover:bg-[#f46174] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] mt-5 md:mt-[40px] lg:mt-[70px]">Start FREE trial</p>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative"  >
           <img src={homebck} alt="" className="z-10" />
-          <div className="absolute inset-0 flex justify-center items-center flex-col w-[35%] font-light ">
-            <h2 className="z-40 text-white text-center text-5xl">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
-          <p className="text-white px-[34px] cursor-pointer text-2xl font-medium py-[10px] rounded-full bg-[#E13C52] mt-[70px] hover:bg-[#f46174] ">Start FREE trial</p>
+          <div className="lg:absolute md:absolute sm:static inset-0 flex  justify-center items-center flex-col lg:w-[35%] sm:w-[100%] md:w-[55%] px-9 sm:mt-5   mt-5 w-full font-light">
+          <h2 className="z-40 text-white text-center lg:text-5xl text-[30px] md:text-[35px] sm:text-[35px]">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
+          <p className="text-white px-[34px] cursor-pointer lg:text-2xl  md:text-[20px] sm:text-[20px] font-medium py-[10px] rounded-full bg-[#E13C52] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] hover:bg-[#f46174] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] mt-5 md:mt-[40px] lg:mt-[70px]">Start FREE trial</p>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative"  >
           <img src={homebck} alt="" className="z-10" />
-          <div className="absolute inset-0 flex justify-center items-center flex-col w-[35%] font-light ">
-            <h2 className="z-40 text-white text-center text-5xl">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
-          <p className="text-white px-[34px] cursor-pointer text-2xl font-medium py-[10px] rounded-full bg-[#E13C52] mt-[70px] hover:bg-[#f46174] ">Start FREE trial</p>
+          <div className="lg:absolute md:absolute sm:static inset-0 flex  justify-center items-center flex-col lg:w-[35%] sm:w-[100%] md:w-[55%] px-9 sm:mt-5   mt-5 w-full font-light">
+          <h2 className="z-40 text-white text-center lg:text-5xl text-[30px] md:text-[35px] sm:text-[35px]">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
+          <p className="text-white px-[34px] cursor-pointer lg:text-2xl  md:text-[20px] sm:text-[20px] font-medium py-[10px] rounded-full bg-[#E13C52] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] hover:bg-[#f46174] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] mt-5 md:mt-[40px] lg:mt-[70px]">Start FREE trial</p>
           </div>
         </div>
-        <div className="relative">
+        <div className="relative"  >
           <img src={homebck} alt="" className="z-10" />
-          <div className="absolute inset-0 flex justify-center items-center flex-col w-[35%] font-light ">
-            <h2 className="z-40 text-white text-center text-5xl">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
-          <p className="text-white px-[34px] cursor-pointer text-2xl font-medium py-[10px] rounded-full bg-[#E13C52] mt-[70px] hover:bg-[#f46174] ">Start FREE trial</p>
+          <div className="lg:absolute md:absolute sm:static inset-0 flex  justify-center items-center flex-col lg:w-[35%] sm:w-[100%] md:w-[55%] px-9 sm:mt-5   mt-5 w-full font-light">
+          <h2 className="z-40 text-white text-center lg:text-5xl text-[30px] md:text-[35px] sm:text-[35px]">Dive into a <span className="font-bold">universe</span> of un-ending content and channels</h2>
+          <p className="text-white px-[34px] cursor-pointer lg:text-2xl  md:text-[20px] sm:text-[20px] font-medium py-[10px] rounded-full bg-[#E13C52] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] hover:bg-[#f46174] lg:mt-[70px] md:mt-[40px] sm:mt-[15px] mt-5 md:mt-[40px] lg:mt-[70px]">Start FREE trial</p>
           </div>
         </div>
+       
+       
       </Slider>
     </div>
     <RecommendedChannels/>
@@ -71,7 +73,7 @@ const CustomNextArrow = (props) => {
   const { onClick } = props;
   return (
     <button
-      className="absolute bottom-4 hover:bg-white hover:text-black ml-20 left-1/2 transform -translate-x-0 z-20 bg-transparent text-white border flex rounded-full w-10 h-10 items-center justify-center"
+      className="absolute  bottom-4 hover:bg-white hover:text-black ml-20 left-1/2 transform -translate-x-0 z-20 bg-transparent text-white border  rounded-full w-10 h-10 sm:hidden md:flex lg:flex xl:flex hidden items-center justify-center"
       onClick={onClick}
     >
       <GrNext />
@@ -83,7 +85,7 @@ const CustomPrevArrow = (props) => {
   const { onClick } = props;
   return (
     <button
-      className="absolute mr-20 bottom-4 right-1/2 transform translate-x-0 z-20 bg-transparent text-white border hover:bg-white hover:text-black flex rounded-full w-10 h-10  items-center justify-center"
+      className="absolute mr-20 bottom-4 right-1/2 transform translate-x-0 z-20 bg-transparent text-white border hover:bg-white hover:text-black flex rounded-full w-10 h-10 sm:   md:flex lg:flex xl:flex hidden   items-center justify-center"
       onClick={onClick}
     >
       <GrPrevious />
